efbd3f2c61c913c4923574bb6c1504fab8ddf2a9
b5710546232 <nattapat.su@ku.th>
2016-11-06 21:36:07 +0700

    rice reg

--- writer/rice_fact_writer.py
+++ writer/rice_fact_writer.py
@@ -10,9 +10,9 @@ output.write(':- module(rice_fact, []).\n')
 rice_list = []
 for row in reader:
     temp = row['Rice name']
-    temp = temp.replace(' ','_')
-    temp = temp.replace('-','_')
-    temp = temp.replace('.','')
+    temp = temp.replace(' ','')
+    temp = temp.replace('-','')
+    # temp = temp.replace('.','')
     rice_fact = "rice(%s)."%(temp)
     rice_list.append(rice_fact)
     print(rice_fact)
--- writer/rice_region_fact_writer.py
+++ writer/rice_region_fact_writer.py
@@ -1,19 +1,35 @@
 import csv
 
-f = open('../data/KEM.csv','r')
+f = open('../data/KEM02.csv','r')
 output = open('../src/rice_region_fact.pl','w')
 reader = csv.DictReader(f)
 
 # :- module(rice_fact, []).
 output.write(':- module(rice_region_fact, []).\n')
-
-fact_list = []
+all_reg =["ภาคตะวันออกเฉียงเหนือ","ภาคกลาง","ภาคใต้","ภาคตะวันตก","ภาคเหนือ","ภาคตะวันออก"]
+result_list = []
 for row in reader:
-    temp = row['region']
-    temp = temp.replace(' ','_')
-    temp = temp.replace('-','_')
-    temp = temp.replace('.','')
-    fact = temp
-    fact_list.append(fact)
-    print(fact)
-    output.write(fact+"\n")
+    rice = row['Rice name']
+    rice = rice.replace(' ','')
+    rice = rice.replace('-','')
+    # rice = rice.replace('.','')
+
+    regions = row['region']
+    regions = regions.split(" ")
+
+    for reg in regions:
+        if(reg != ''):
+            reg = reg.replace("ตอนบน",'')
+            reg = reg.replace("ล่าง",'')
+            reg = reg.replace("บน",'')
+            reg = reg.replace("ตอนล่าง",'')
+            if(reg=='ทุกภาค'):
+                for r in all_reg:
+                    fact = "grow_in(%s,%s)."%(rice,r)
+                    print(fact)
+                    result_list.append(fact)
+            else:
+                fact = "grow_in(%s,%s)."%(rice,reg)
+                print(fact)
+                result_list.append(fact)
+    # output.write(rice_fact+"\n")
