671cfa23ad30b016de259dc39214ddaa7d970970
b5710546232 <nattapat.su@ku.th>
2016-11-06 12:40:22 +0700

    rice_fact

---
+++ writer/rice_fact_generate.py
@@ -0,0 +1,19 @@
+import csv
+
+f = open('../data/KEM.csv','r')
+output = open('../src/rice_fact.pl','w')
+reader = csv.DictReader(f)
+
+# :- module(rice_fact, []).
+output.write(':- module(rice_fact, []).\n')
+
+rice_list = []
+for row in reader:
+    temp = row['Rice name']
+    temp = temp.replace(' ','_')
+    temp = temp.replace('-','_')
+    temp = temp.replace('.','')
+    rice_fact = "rice(%s)"%(temp)
+    rice_list.append(rice_fact)
+    print(rice_fact)
+    output.write(rice_fact+"\n")
