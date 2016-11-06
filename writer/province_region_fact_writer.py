3b4da18ee9ddf7662952273e3cfd499c46fb6ed4
b5710546232 <nattapat.su@ku.th>
2016-11-06 15:31:11 +0700

    has_reg

--- writer/province_region_fact_writer.py
+++ writer/province_region_fact_writer.py
@@ -1,27 +1,27 @@
 import csv
 
 f = open('../data/list_distinct.csv','r')
-output = open('../src/provice_region_fact.pl','w')
+output = open('../src/province_region_fact.pl','w')
 reader = csv.DictReader(f)
 
 # :- module(rice_fact, []).
-output.write(':- module(provice_region_fact, []).\n')
+output.write(':- module(province_region_fact, []).\n')
 
 result_list = []
 for row in reader:
     temp2 = row['Sr.No']
     try:
         if(int(temp2)>0):
-            provice = row['Province (Changwat).']
-            provice = provice.replace(' ','_')
-            provice = provice.replace('-','_')
-            provice = provice.replace('.','')
+            province = row['Province (Changwat).']
+            province = province.replace(' ','_')
+            province = province.replace('-','_')
+            province = province.replace('.','')
             region = row['Region']
             region = region.replace(' ','_')
             region = region.replace('-','_')
             region = region.replace('.','')
 
-            fact = "has(%s,%s)."%(provice.lower(),region.lower())
+            fact = "has_region(%s,%s)."%(province.lower(),region.lower())
             print(fact)
             if (fact not in result_list):
                 result_list.append(fact)
