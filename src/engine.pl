17beb694da675971de0d1ca388fad75711c32688
b5710546232 <nattapat.su@ku.th>
2016-11-08 23:54:57 +0700

    fix engine writer

--- src/engine.pl
+++
@@ -1,4 +0,0 @@
-:- load_files(province_fact, [encoding(utf8)]).
-:- load_files(province_region_fact, [encoding(utf8)]).
-:- load_files(rice_fact, [encoding(utf8)]).
-:- load_files(rice_region_fact, [encoding(utf8)]).
--- writer/engine_writer.py
+++ writer/engine_writer.py
@@ -4,12 +4,12 @@ file_list = []
 output = open('../src/engine.pl','w')
 for root, dirs, files in os.walk("../src"):
     for file in files:
-        if file != 'engine.pl':
+        if file != 'engine.pl' and root=='../src':
             target = file.replace('.pl','')
             file_list.append(target)
-            # print(target)
-
+            # print(root)
+            print(target)
 for item in file_list:
-
+    pass
     output.write(':- load_files(%s, [encoding(utf8)]).\n'%(item))
 print('finish create engine.pl ...')
