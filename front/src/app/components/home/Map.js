3ec38ba9b6a5eb6a6cdeec8561a54d8b029ba70c
ReiiYuki <iguru.shou.5410070@gmail.com>
2016-11-10 23:36:19 +0700

    Current Location

--- front/src/app/components/home/Map.js
+++ front/src/app/components/home/Map.js
@@ -5,17 +5,33 @@ class Map extends Component {
 
   createMap() {
     this.map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -34.397, lng: 150.644},
-        zoom: 8
-    });
+      center: {lat: 13.0069792, lng: 102.2699603},
+      zoom: 6
+    })
+    this.setCenter()
   }
 
+  setCenter(){
+    let current_map = this.map
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        }
+        current_map.setCenter(pos)
+        current_map.setZoom(10)
+      }, function() {
+        handleLocationError(true,  current_map.getCenter());
+      })
+    }
+  }
   componentDidMount() {
     this.createMap();
   }
 
   render(){
-    var style={
+    let style={
       position:'absolute',
       height:'100%',
       width:'100%'
