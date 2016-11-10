98d4404f34af03ed2e584765ba0f28a895c1648f
ReiiYuki <iguru.shou.5410070@gmail.com>
2016-11-10 23:26:07 +0700

    Google Map

--- front/src/app/components/home/Home.js
+++ front/src/app/components/home/Home.js
@@ -3,42 +3,18 @@ import { Link } from 'react-router'
 import {Input,Button,Table} from 'react-materialize'
 import {loadRice} from '../../actions/RiceAction'
 import {connect} from 'react-redux'
+import Map from './Map'
+
 class Home extends Component {
   onInput(e){
-    e.preventDefault()
+    e.preventDefault();
     let data = { province:this.refs.form.province.value }
-    this.props.loadRice(data)
+    this.props.loadRice(data);
   }
+
   render() {
     return (
-      <div className="container center">
-        <form ref="form">
-          <Input s={12} type="text" name="province" label="Input Province" onChange={(e)=>this.onInput(e)}></Input>
-        </form>
-        {this.props.rices.length>0?
-          <Table>
-            <thead>
-              <tr>
-                <th data-field="id">Rice</th>
-                <th data-field="name">Harvest Season</th>
-                <th data-field="price">Region</th>
-              </tr>
-            </thead>
-
-            <tbody>
-              this.props.rices.map((rice)=>(
-                <tr>
-                  <td>rice.name</td>
-                  <td>rice.harvest_season</td>
-                  <td>rice.region</td>
-                </tr>
-              ))
-            </tbody>
-          </Table>:
-          <div></div>
-        }
-
-      </div>
+      <Map/>
     )
   }
 }
