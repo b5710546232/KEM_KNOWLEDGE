2967c97b644dc410315e29f2759cfeb991134986
ReiiYuki <iguru.shou.5410070@gmail.com>
2016-11-11 14:03:05 +0700

    Static Modal

--- front/src/app/components/home/RiceInfomationModal.js
+++ front/src/app/components/home/RiceInfomationModal.js
@@ -6,20 +6,61 @@ class RiceInfomationModal extends Component {
     $('#rice-modal').modal()
   }
   render(){
+    console.log(this.props.location);
     return(
       <div>
         <div id="rice-modal" className="modal modal-fixed-footer">
           <div className="modal-content">
-            <h4>Rice Variety Suggested Information</h4>
+            <div className="row">
+              <h4>Rice Suggestion - {this.props.location.sub_district}, {this.props.location.province}</h4>
+            </div>
+            <div className="row">
+              <div className="col s8">
+                <div className="row">
+                  <div className="col s12">
+                    <h5>Location</h5>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col s6">
+                    <strong>Sub-District</strong>
+                  </div>
+                  <div className="col s6">
+                    {this.props.location.sub_district}
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col s6">
+                    <strong>District</strong>
+                  </div>
+                  <div className="col s6">
+                    {this.props.location.district}
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col s6">
+                    <strong>Province</strong>
+                  </div>
+                  <div className="col s6">
+                    {this.props.location.province}
+                  </div>
+                </div>
+              </div>
+              <div className="col s4 ">
+                <img
+                  className="circle
+                  responsive-img right"
+                  src={this.props.location.lat?
+                    "http://maps.googleapis.com/maps/api/staticmap?center="+this.props.location.lat+","+this.props.location.lng+"&zoom=15&maptype=hybrid&size=200x200&key=AIzaSyBfW5XODIw5cTc84x4h8lL1qhB7EbLPhX0"
+                    :""
+                    }>
+                </img>
+              </div>
+            </div>
             <p>Data Here</p>
-            <p>
-              Sub-District - "{this.props.location.sub_district}"
-              District - "{this.props.location.district}"
-              Province - "{this.props.location.province}"
-            </p>
           </div>
           <div className="modal-footer">
-            <a href="#!" className=" modal-action modal-close waves-effect waves-green btn-flat">Close</a>
+            <a className=" modal-action modal-close waves-effect waves-green btn-flat">Close</a>
           </div>
         </div>
       </div>
