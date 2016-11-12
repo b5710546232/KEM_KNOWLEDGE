8da46ffbaa224e7ce0a399ceae8fa7f86c5f78ff
gafig <chawakorn.a@ku.th>
2016-11-12 21:51:29 +0700

    expert rule ver 0.1

--- src/rules.pl
+++ src/rules.pl
@@ -30,4 +30,16 @@ simpleRule(Rice,RiceType,SubDis,Disrict,Province,Price,SellPlace,Humidity,Season
   district_class:is_part_of(Disrict,Province),
   ricetype_price:sold_for(RiceType,SpecialCase,Pro,Humidity,Price).
 
-%expertRule(Rice,RiceType,SubDis,Disrict,Province,,Price,SellPlace,Season) :-
+expertRule(Rice,RiceType,SubDis,Disrict,Province,Price,SellPlace,Humidity,Season,PhotoPeriod,Pest) :-
+  province_region_fact:has_region(Province,Region),
+  rice_growth:grows_well_in(Rice,SubDis,Season),
+  rice_properties:has_properties(Rice,RiceType,Yield,PhotoPeriod,SpecialCase1),
+  province_region_fact:has_region(Pro,Region),
+  sub_district_class:is_part_of(SubDis,Disrict),
+  district_class:is_part_of(Disrict,Province),
+  pest_class:is_subclass_of(P,pest),
+  (insect_class:is_instance_of(Pest,P) ; disease_class:is_instance_of(Pest,P)),
+  (P == insect -> (rice_vulnerable_to_insect:vulnerable_to(Rice,Pest) -> P==P; Pest='') ; (rice_vulnerable_to_disease:vulnerable_to(Rice,Pest) -> P==P; Pest='')),
+  %(P == disease -> (rice_vulnerable_to_disease:vulnerable_to(Rice,Pest) -> Pest; P==P) ; P==P),
+  ricetype_price:sold_for(RiceType,SpecialCase2,Pro,Humidity,Price).
+%listTrav([H|T]) :- process(H), listTrav(T).
